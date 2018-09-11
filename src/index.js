c3c57f78ed1b0c86b60742cc85f43677f38832fa
ton9q <kuchma_anton@mail.ru>
2018-09-11 19:31:12 +0300

    addiction of logic

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
+
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var loveTriangles = 0;
+
+  for(let i = 0; i < preferences.length; i++) {
+    if(i + 1 === preferences[preferences[preferences[i] - 1] - 1] && 
+        preferences[i] !== preferences[preferences[i] - 1]) {
+      loveTriangles++;
+    }
+  }
+
+  return loveTriangles / 3
 };
